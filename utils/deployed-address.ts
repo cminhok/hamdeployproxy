583a6de97be5ef2a31652b83859f92b321cdf579
chang min hok <cminhok@gmail.com>
2024-08-16 13:38:22 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x1E10b0f75728b197024979D386514EbDc0600972'
+export const ProxyAdmin = '0x31Fdc0E01E0AF7FFa2dC4F0451eBc5815c3a1744'
+export const SWTRImplementation = '0x8CE496F5687CB3D9781cB52bCaF9774855137A5a'
     
